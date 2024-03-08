26ea42817db779a4b27c6981a7ba7fb35c0ff419
Amanturov7 <edi.amanturov@gmail.com>
2024-03-09 00:37:47 +0600

    maps demo

--- lib/screen/maps_page.dart
+++ lib/screen/maps_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MapsPage extends StatelessWidget {
   @override
@@ -7,8 +10,17 @@ class MapsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Карты'),
       ),
-      body: Center(
-        child: Text('Это страница с картами'),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(51.509364, -0.128928),
+          zoom: 9.2,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+        ],
       ),
     );
   }
