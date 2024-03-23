ec1ecdd88e1f0ec4d5a1522673459f7deda53916
Amanturov7 <edi.amanturov@gmail.com>
2024-03-24 02:49:15 +0600

    user_data all

--- lib/user_data_page.dart
+++ lib/user_data_page.dart
@@ -11,9 +11,36 @@ class UserDataPage extends StatefulWidget {
 }
 
 class _UserDataPageState extends State<UserDataPage> {
-  int id = 0;
   String username = '';
+  String passportSerial = '';
   String email = '';
+  String role = '';
+  String address = '';
+  DateTime? signupDate;
+  BigInt? phone;
+  BigInt? inn;
+
+
+
+
+  bool isLoading = false;
+  bool isEditing = false;
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+  final _formKey = GlobalKey<FormState>(); // Ключ формы для валидации и сохранения данных
 
   @override
   void initState() {
@@ -22,6 +49,10 @@ class _UserDataPageState extends State<UserDataPage> {
   }
 
   Future<void> fetchData() async {
+    setState(() {
+      isLoading = true;
+    });
+
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('token');
     if (token != null) {
@@ -32,11 +63,20 @@ class _UserDataPageState extends State<UserDataPage> {
         },
       );
       if (response.statusCode == 200) {
-        final userData = jsonDecode(response.body);
+        final userData = json.decode(utf8.decode(response.bodyBytes));
         setState(() {
-          id = userData['id'] ?? 0;
           username = userData['username'] ?? '';
           email = userData['email'] ?? '';
+          passportSerial =  userData['passportSerial'] ?? '';
+          role =  userData['role'] ?? '';
+          address =  userData['address'] ?? '';
+signupDate = DateTime.tryParse(userData['signupDate'] ?? '');
+        phone = BigInt.tryParse(userData['phone'].toString()) ?? BigInt.zero;
+inn = BigInt.tryParse(userData['inn'].toString()) ?? BigInt.zero;
+
+          
+          isLoading = false;
+
         });
       } else {
         throw Exception('Failed to load user data');
@@ -44,22 +84,238 @@ class _UserDataPageState extends State<UserDataPage> {
     }
   }
 
+
+
+void handleSubmit(){
+   if (_formKey.currentState!.validate()) {
+
+   }
+}
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('my_details'.tr()),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () {
+              setState(() {
+                isEditing = true;
+              });
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('ID: $id'),
-            Text('Имя пользователя: $username'),
-            Text('Email: $email'),
-          ],
-        ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+
+                      SizedBox(height: 8),
+    Text(
+      'Адрес', // Метка поля
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
       ),
+    ),
+
+                      TextFormField(
+                        initialValue: '$address',
+                        readOnly: true,
+                         validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите адрес';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'username'.tr(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF3BB5E9)),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                      ),
+                      SizedBox(height: 8),
+    Text(
+      'Логин', // Метка поля
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    ),
+
+                      SizedBox(height: 8),
+                      TextFormField(
+                        initialValue: username,
+                        readOnly: !isEditing,
+
+                           validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите имя пользователя';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'username'.tr(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF3BB5E9)),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                      ),
+
+
+SizedBox(height: 8),
+    Text(
+      'ИНН', // Метка поля
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    ),
+
+                  SizedBox(height: 8),
+                      TextFormField(
+                        initialValue: inn?.toString(),
+                        readOnly: !isEditing,
+
+                           validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите имя inn';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'inn'.tr(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF3BB5E9)),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                      ),
+
+SizedBox(height: 8),
+    Text(
+      'phone_number'.tr(), // Метка поля
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    ),
+
+                      
+                  SizedBox(height: 8),
+                      TextFormField(
+                        initialValue: phone?.toString(),
+                        readOnly: !isEditing,
+
+                           validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите имя phone';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'phone'.tr(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF3BB5E9)),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                      ),
+SizedBox(height: 8),
+    Text(
+      'passport_serial'.tr(), // Метка поля
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    ),
+
+                        SizedBox(height: 8),
+                      TextFormField(
+                        initialValue: passportSerial,
+                        readOnly: !isEditing,
+
+                           validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите  passportSerial';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'passport_serial'.tr(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF3BB5E9)),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                      ),
+SizedBox(height: 8),
+    Text(
+      'Email', // Метка поля
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    ),
+
+
+                        SizedBox(height: 8),
+                      TextFormField(
+                        initialValue: email,
+                        readOnly: !isEditing,
+
+                           validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите имя пользователя';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF3BB5E9)),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
     );
   }
 }
+
